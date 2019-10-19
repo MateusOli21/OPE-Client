a2957ad0196786965c4f542401e357bb4f9ac8c7
SalehKhalil <junior.khalil@hotmail.com>
2019-10-18 23:33:08 -0300

    Tab show color with selected

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -3,10 +3,9 @@
     <template>
       <v-container class="grey lighten-5" mt-5 mb-3>
         <!-- TODO; FAZER COM QUE O PROFESSOR VISUALIZE O COMPONENTE GROUPTAB COM OS DADOS DO GRUPO, AO INVÉS DE SER REDIRECIONADO A UMA OUTRA ROTA -->
-        <!-- TODO; AO CLICAR NA ABA DESEJADA, DEIXA-LA COM UM ESTILO DE MARCAÇÃO -->
         <v-row no-gutters align="center">
           <v-col v-for="(menu, index) in menuItem" :key="index" cols="12" sm="4">
-            <v-btn @click="selectTab(menu.name)" block text large>{{ menu.name }}</v-btn>
+            <v-btn @click="selectTab(menu.name)" :class="{'selectTab': menu.name === actualTab}" block text large>{{ menu.name }}</v-btn>
           </v-col>
         </v-row>
         <v-divider></v-divider>
@@ -24,6 +23,7 @@ import ButtonBackToHome from "./TeacherPage/ButtonBackToHome";
 export default {
   data() {
     return {
+      actualTab: "Grupo",
       isGropDetails: !!localStorage.getItem("groupDetails"),
       menuItem: [{ name: "Grupo" }, { name: "Sprint" }, { name: "Notas" }]
     };
@@ -34,6 +34,7 @@ export default {
   methods: {
     selectTab(tab) {
       this.$emit("update:tabSelected", tab);
+      this.actualTab = tab
     }
   },
   components: {
@@ -47,5 +48,8 @@ export default {
   display: flex;
   justify-content: flex-start;
   padding: 20PX 20px 0 0;
+}
+.selectTab {
+  background-color: #e4e5e6;
 }
 </style>
