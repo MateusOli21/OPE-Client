12a3544686ddee4f89be6ef4dde2ec4ac92a8835
SalehKhalil <junior.khalil@hotmail.com>
2019-11-25 22:45:37 -0300

    Improvement main menu

---
+++ src/components/TeacherPage/GradeTab.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col sm="4"><h2>Critérios de Avaliação</h2></v-col>
+      <v-col sm="4"></v-col>
+      <v-col sm="4"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+</style>
