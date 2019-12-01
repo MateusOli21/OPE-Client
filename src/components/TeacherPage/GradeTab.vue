995571ca247830f52c56f7106775280dcf3716dc
SalehKhalil <junior.khalil@hotmail.com>
2019-12-01 20:22:06 -0300

    Componentizing the criterion block

--- src/components/TeacherPage/GradeTab.vue
+++ src/components/TeacherPage/GradeTab.vue
@@ -6,16 +6,7 @@
           <v-card-title class="title">Critérios de Avaliação</v-card-title>
           <v-container class="lighten-5">
             <v-row no-gutters class="center-x-col">
-              <div class="criteria-container">
-                <v-text-field outlined label="Critério" prepend-icon="mdi-playlist-check" required></v-text-field>
-                <v-text-field
-                  outlined
-                  label="Peso"
-                  prepend-icon="mdi-weight-kilogram"
-                  required
-                  :rules="[() => groupName.length > 0 || 'Campo obrigatório']"
-                ></v-text-field>
-              </div>
+              <CriteriaContainer v-for="criteria in criterias" :key="criteria" />
             </v-row>
             <v-row class="center-x-col my-5">
               <v-btn fab small dark color="indigo">
@@ -30,18 +21,32 @@
 </template>
 
 <script>
+import CriteriaContainer from "./CriteriaContainer";
+
+export default {
+  data() {
+    return {
+      criterias: [
+        {
+          criteria: "",
+          weight: ""
+        }
+      ]
+    };
+  },
+  components: {
+    CriteriaContainer
+  },
+  methods: {
+    createAnotherCriterion() {}
+  }
+};
 </script>
 
 <style lang="scss">
 .grade {
   font-family: Roboto, sans-serif !important;
   font-weight: 400 !important;
-}
-
-.criteria-container {
-  padding: 40px 40px 10px 20px;
-  width: 60%;
-  border: 1.4px dashed grey;
 }
 
 .center-x-col {
